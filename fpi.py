e82ce235b7c20d0d0ddf465a59d8baf2c6470762
andreasoscar <seolmi@hotmail.se>
2021-01-22 10:13:14 +0100

    fpi

--- fpi.py
+++ fpi.py
@@ -1,10 +1,17 @@
 from math import *
-def fpi(f,g,gprime,x0,tol):
+import math
+def fpi(f,g,gprime,x0,tol,step):
     x = x0
-    if abs(eval(gprime)) > 1:
+    k = 0
+    if abs(eval(gprime)) >= 1:
         print("Not a valid guess")
     else:
         while abs(eval(f)) > tol:
-            x = eval(g)
+            if k >= step:
+                break
+            else:
+                k += 1
+                x = eval(g)
         print("x = " + ("{:." + str(8) + "f}").format(x))
-fpi("exp(x) + sin(x) - 4", "log(4-sin(x))", "-cos(x)/(4-sin(x))",2,0.00000001)
+fpi("-4x-0", "0", "-4",0,0.00000001)
+#fpi("exp(x) + sin(x) - 4", "log(4-sin(x))", "-cos(x)/(4-sin(x))",2,0.00000001)
