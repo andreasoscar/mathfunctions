71e5bec60f224ba49f1e78c9a96e89d51e497a9e
Andreas Olsson <andreasolsson@MacBook-Pro-som-tillhor-Andreas.local>
2021-01-30 10:44:57 +0100

    j

---
+++ jacobian.py
@@ -0,0 +1,29 @@
+import math
+import numpy as np
+<<<<<<< HEAD
+p = [] #true error array
+x_n = np.matrix([1,2,3]).transpose()
+r = np.matrix([1,1,1]).transpose()
+for i in range(10):
+    x,y,z = x_n.item(0),x_n.item(1),x_n.item(2)
+    F = np.matrix([[eval("x**2 - 2*x + y**2 - z + 1")],[eval("x*(y**2) - x - 3*y + y*z + 2")],[eval("x*(z**2)-3*z + y*(z**2) + x*y")]])
+    J = np.matrix([[eval("2*x - 2"), eval("2*y"), eval("-1")],[eval("y**2 - 1"), eval("2*x*y - 3 + z"), eval("y")],[eval("z**2 + y"), eval("x + z**2"), eval("2*x*z + 2*y*z - 3")]])
+    x_n = x_n - (J.I)*F
+    e = math.sqrt((r.item(0)-x_n.item(0))**2 + (r.item(1)-x_n.item(1))**2 + (r.item(2) - x_n.item(2))**2)
+    print(x_n.item(0),x_n.item(1),x_n.item(2),e)
+    p.append(e)
+for i in range(0,len(p)-1):
+    print(p[i+1]/p[i])
+print(x_n)
+=======
+#x_1 = x, x_2 = y, x_3 = z
+x,y,z = 1,2,3
+F = np.matrix([[eval("x**2 - 2*x + y**2 - z + 1")],[eval("x*(y**2) - x - 3*y + y*z + 2")],[eval("x*(z**2)-3*z + y*(z**2) + x*y")]])
+J = np.matrix([[eval("2*x - 2"), eval("2*y"), eval("-1")],[eval("y**2 - 1"), eval("x - z - 3"), eval("y")],[eval("z**2 + y"), eval("x + z**2"), eval("2*x*z + 2*y*z - 3")]])
+J_inv = np.linalg.inv(J)
+x = np.matrix([x,y,z]).transpose()
+for i in range(10):
+    x,y,z = x.item(0),x.item(1),x.item(2)
+    x = x - np.dot(J_inv,F)
+print(x)
+>>>>>>> 5488a2f7cf7cfc65c74396e5deae177dd68bb089
