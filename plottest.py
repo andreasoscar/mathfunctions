9f7e338b661115a36e79955123842550841ad4c9
Andreas Olsson <andreasolsson@MacBook-Pro-som-tillhor-Andreas.local>
2021-02-02 19:02:50 +0100

    r

--- plottest.py
+++ plottest.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_DOWN
 import random
 data = []
+new = []
 while len(data) < 300:
     x = round(random.uniform(-1,1),2)
     y = round(random.uniform(-1,1),2)
     if abs(x)+abs(y) == 1:
         new_x = x + 2*y
         new_y = 2*x + y
+        new.append([x,y])
         data.append([new_x,new_y])
 x, y = np.array(data).T
+plt.scatter(x,y)
+x, y = np.array(new).T
 plt.scatter(x,y)
 plt.show()
